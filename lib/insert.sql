5468f5ef9085057570671c8045a4905ef6f77cce
embraman10@gmail.com <embraman10@gmail.com>
2020-07-01 14:45:07 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "chocolate", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "brown", "nice", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "albino", "grumpy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "black", "tired", 1);
+INSERT INTO bears VALUES (7,"Wendy", 12, "F", "honey", "naive", 0);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", 0);
