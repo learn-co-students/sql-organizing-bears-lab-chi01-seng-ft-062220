6482e8c971e83f34202e15da8cf55e0ec21eb386
foxten <tenaysiafox@gmail.com>
2020-07-01 11:17:59 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+-- name TEXT,
+--     age INTEGER,
+--     gender TEXT,
+--     color TEXT,
+--     temperament TEXT,
+--     alive BOOLEAN
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, 'M', "Brown", "Kind", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, 'M', "Black", "Sweet", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, 'F', "Brown", "Quiet", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 3, 'M', "Calm", "kind", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, 'F', "Black", "Hyper", "false");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 1, 'M', "Blue", "Moody", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 4, 'F', "Black", "Nervous", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 6, 'M', "Black", "Angry", "true");
