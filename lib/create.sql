6193abe44a96b0d38ee725a01984b1a03a8cd537
J-Waz <jgw.jakegeorge@gmail.com>
2020-07-03 17:35:12 -0500

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,8 @@
-  
 CREATE TABLE bears (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER,
-  gender CHAR(1),
+  gender TEXT,
   color TEXT,
   temperament TEXT,
   alive BOOLEAN
--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,23 @@
-INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
-INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "intense", 1);
-INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "Nice", 0);
-INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 1);
-INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark brown", "goofy", 1);
-INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
-INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Blue", "naive", 0);
-INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) 
+VALUES ("Akakabuto", 56, "M", "Red", "Bloodthirsty", false, 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) 
+VALUES ("Andy Panda", 6, "M", "Oreo", "Playfull", true, 2);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) 
+VALUES ("Baloo", 80, "M", "Burnt Umber", "goofy", true, 3);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) 
+VALUES ("Barnacles", 999, "M", "Green", "Wise", false, 4);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) 
+VALUES ("Barney Bear", 45, "M", "Blue", "goofy", true, 5);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) 
+VALUES ("Kenai", 15, "F", "Transparent", "Vengeful", false, 6);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) 
+VALUES ("Umka", 16, "F", "White", "Friendly", true, 7);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) 
+VALUES (NULL, 1, "M", "Blue", "Naive", true, 8);
