a13074dd40d3ee25e676801215608e14ff537ad2
Sam M <sammontalvojr@gmail.com>
2020-07-05 20:10:41 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, gender, age, alive) VALUES ("Mr. Chocolate", "M", 4, 1);
+INSERT INTO bears (name, gender, age, alive) VALUES ("Rowdy", "F", 7, 1);
+INSERT INTO bears (name, gender, age, alive) VALUES ("Tabitha", "M", 4, 0);
+INSERT INTO bears (name, gender, age, alive) VALUES ("Sergeant Brown", "F", 6, 1);
+INSERT INTO bears (name, gender, age, alive) VALUES ("Melissa", "M", 24, 1);
+INSERT INTO bears (name, gender, age, alive) VALUES ("Grinch", "F", 23, 0);
+INSERT INTO bears (name, gender, age, alive) VALUES ("Wendy", "M", 43, 0);
+INSERT INTO bears (name, gender, age, alive) VALUES (null, "F", 90, 1);
